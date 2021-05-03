3222296472e814e35e80822a92568d67bedd6ab1
Abubakr Olorunlambe <abubakrolorunlambe@gmail.com>
2021-05-03 15:11:57 +0100

    Implemented a service called shared that contains method that interface with external API. Also implementeda component called catalogue which shows all the books in the bookstore's possession

--- Scientia-Application/src/app/app-routing.module.ts
+++ Scientia-Application/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CatalogueComponent } from './catalogue/catalogue.component';
+
+const routes: Routes = [
+  { path: 'catalogue', component: CatalogueComponent, pathMatch: 'full' }
+  //{ path: 'addbook', component: AddBookComponent, pathMatch: 'full' }
+  //{path:'authors', component: AuthorComponent, pathMatch: 'full' }
+  //{path:'book-update-delete', component}
+  //{path:'author-update-delete', component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
