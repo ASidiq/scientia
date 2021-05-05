711f911ae3315e51aefdbf28550032c475ad48da
Abubakr Olorunlambe <abubakrolorunlambe@gmail.com>
2021-05-05 14:42:51 +0100

    Implemented the component that handles the creation of new books. Cleaned the nav-menu component

--- Scientia-Application/src/app/catalogue/catalogue.component.ts
+++ Scientia-Application/src/app/catalogue/catalogue.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ElementRef, HostListener, AfterViewInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { MdbTableDirective, MdbTablePaginationComponent } from 'angular-bootstrap-md';
-import { HttpClient } from '@angular/common/http';
 import { SharedService } from '../shared.service'
 
 @Component({
@@ -14,7 +13,6 @@ export class CatalogueComponent implements OnInit, AfterViewInit {
   @ViewChild('row', { static: true }) row: ElementRef;
 
   elements: any = [];
-  catalogue: any = [];
   headElements = ['id', 'cover', 'title', 'author', 'date', 'type', 'genre', 'rating', 'pages', 'copies'];
 
   searchText: string = '';
@@ -25,7 +23,7 @@ export class CatalogueComponent implements OnInit, AfterViewInit {
   url: string = 'https://jsonplaceholder.typicode.com/posts';
   readonly APIUrl: string = 'https://localhost:5001/api';
 
-  constructor(private cdRef: ChangeDetectorRef, private http: HttpClient, private service: SharedService) { }
+  constructor(private cdRef: ChangeDetectorRef, private service: SharedService) { }
 
   @HostListener('input') oninput() {
     this.mdbTablePagination.searchText = this.searchText;
@@ -35,25 +33,10 @@ export class CatalogueComponent implements OnInit, AfterViewInit {
   //Populating array which will be used as datasource
   ngOnInit(): void {
     this.refreshCatalogueList();
-
-    // this.http.get(this.url).subscribe((data: any) => {
-    //   data.forEach((el: any) => {
-    //     this.elements.push({
-    //       id: el.id.toString(),
-    //       first: el.title,
-    //       last: el.body,
-    //       handle: 'Handle ' + el.id.toString()
-    //     });
-    //   });
-    //   this.mdbTable.setDataSource(this.elements);
-    // });
-    // this.elements = this.mdbTable.getDataSource();
-    // this.previous = this.mdbTable.getDataSource();
   }
 
   refreshCatalogueList() {
     this.service.getEntireCatalogue().subscribe(data => {
-      this.catalogue = data;
       data.forEach((el: any) => {
         this.elements.push({
           id: el.ID.toString(),
@@ -83,49 +66,6 @@ export class CatalogueComponent implements OnInit, AfterViewInit {
     this.mdbTablePagination.calculateLastItemIndex();
     this.cdRef.detectChanges();
   }
-
-  // addNewRow() {
-  //   this.mdbTable.addRow({
-  //     id: this.elements.length.toString(),
-  //     first: 'Wpis ' + this.elements.length,
-  //     last: 'Last ' + this.elements.length,
-  //     handle: 'Handle ' + this.elements.length
-  //   });
-  //   this.emitDataSourceChange();
-  // }
-
-  // addNewRowAfter() {
-  //   this.mdbTable.addRowAfter(1, { id: '2', first: 'Nowy', last: 'Row', handle: 'Kopytkowy' });
-  //   this.mdbTable.getDataSource().forEach((el: any, index: any) => {
-  //     el.id = (index + 1).toString();
-  //   });
-  //   this.emitDataSourceChange();
-  // }
-
-  // removeLastRow() {
-  //   this.mdbTable.removeLastRow();
-  //   this.emitDataSourceChange();
-  //   this.mdbTable.rowRemoved().subscribe((data: any) => {
-  //     console.log(data);
-  //   });
-  // }
-
-  // removeRow() {
-  //   this.mdbTable.removeRow(1);
-  //   this.mdbTable.getDataSource().forEach((el: any, index: any) => {
-  //     el.id = (index + 1).toString();
-  //   });
-  //   this.emitDataSourceChange();
-  //   this.mdbTable.rowRemoved().subscribe((data: any) => {
-  //     console.log(data);
-  //   });
-  // }
-
-  // emitDataSourceChange() {
-  //   this.mdbTable.dataSourceChange().subscribe((data: any) => {
-  //     console.log(data);
-  //   });
-  // }
 
   //Search
   searchItems() {
